743d19b09a717141eaec208a3dedcfcac0f19715
Brian Foo <brian@youaremyjoy.org>
2016-09-06 10:03:45 -0400

    add saved items, add date selector

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //=include vendor/jquery-3.1.0.min.js
+//=include vendor/hammer.min.js
 //=include views/header.js
 //=include views/facets.js
 //=include views/search.js
--- src/js/views/search.js
+++ src/js/views/search.js
@@ -6,11 +6,125 @@ var Search = (function() {
   }
 
   Search.prototype.loadListeners = function(){
+    var _this = this;
+
     $('.bib-item-more-link').on('click', function(e){
       e.preventDefault();
       $(this).hide();
       $(this).closest('.bib-items').find('.bib-item').removeClass('more');
     });
+
+    $('.select-checkbox').on('change', function(){
+      var checked = 0;
+      $('.select-checkbox').each(function(i, el){
+        if ($(this).is(':checked')) checked++;
+      });
+      if (checked>0) {
+        $('.saved-items').text(checked + ' saved items').addClass('active');
+      } else {
+        $('.saved-items').removeClass('active');
+      }
+    });
+
+    this.loadTimelineListeners();
+  };
+
+  Search.prototype.loadTimelineListeners = function(){
+    var _this = this;
+    var $timeline = $('#timeline-select');
+    var timeline = new Hammer($timeline[0]);
+    var x0 = 0, x1 = 0, w = 0;
+    var mode = 'normal';
+    this.$lefthandle = $('#timeline-handle-left');
+    this.$righthandle = $('#timeline-handle-right');
+    var lefthandle = new Hammer(this.$lefthandle[0]);
+    var righthandle = new Hammer(this.$righthandle[0]);
+
+    this.$dateStart = $('#input-date-start');
+    this.$dateEnd = $('#input-date-end');
+    this.minYear = parseInt(this.$dateStart.val());
+    this.maxYear = parseInt(this.$dateEnd.val());
+    this.yearRange = this.maxYear - this.minYear;
+
+    righthandle.on('panstart', function(e){
+      mode = 'right';
+    });
+
+    lefthandle.on('panstart', function(e){
+      mode = 'left';
+    });
+
+    timeline.on('panstart', function(e){
+      // console.log(e);
+      w = $timeline.width();
+      x0 = $timeline.offset().left;
+      x1 = e.center.x;
+    });
+
+    timeline.on('panmove', function(e) {
+      // console.log(e);
+      if (mode=='right') _this.onRightHandlePan(x0, e.center.x, w);
+      else if (mode=='left') _this.onLeftHandlePan(x0, e.center.x, w);
+      else _this.onTimelinePan(x0, x1, e.center.x, w);
+    });
+
+    timeline.on('panend', function(e) {
+      // console.log(e);
+      mode = 'normal';
+    });
+  };
+
+  Search.prototype.onLeftHandlePan = function(x0, x1, w) {
+    x1 = x1 - x0;
+    var max = parseFloat(this.$righthandle.attr('data-value'));
+    var w1 = x1/w * 100;
+    if (w1 >= max) w1 = max - 1;
+    this.$lefthandle.width(w1+'%').attr('data-value', w1);
+    var maxYear = Math.round(max / 100 * this.yearRange + this.minYear);
+
+    var d1 = Math.round((x1/w) * this.yearRange + this.minYear);
+    if (d1 < this.minYear) d1 = this.minYear;
+    if (d1 > maxYear) d1 = maxYear;
+    this.$dateStart.val(d1);
+  };
+
+  Search.prototype.onRightHandlePan = function(x0, x2, w) {
+    x2 = x2 - x0;
+    var min = parseFloat(this.$lefthandle.attr('data-value'));
+    var w2 = x2/w * 100;
+    if (w2 <= min) w2 = min+1;
+    w2 = 100-w2;
+    this.$righthandle.width(w2+'%').attr('data-value', 100-w2);
+    var minYear = Math.round(min / 100 * this.yearRange + this.minYear);
+
+    var d2 = Math.round((x2/w) * this.yearRange + this.minYear);
+    if (d2 > this.maxYear) d2 = this.maxYear;
+    if (d2 < minYear) d2 = minYear;
+    this.$dateEnd.val(d2);
+  };
+
+  Search.prototype.onTimelinePan = function(x0, x1, x2, w) {
+    x1 = x1 - x0;
+    x2 = x2 - x0;
+
+    if (x2 < x1) {
+      var tmp = x1;
+      x1 = x2;
+      x2 = tmp;
+    }
+
+    var w1 = x1/w * 100;
+    var w2 = 100 - x2/w * 100;
+
+    this.$lefthandle.width(w1+'%').attr('data-value', w1);
+    this.$righthandle.width(w2+'%').attr('data-value', 100-w2);
+
+    var d1 = Math.round((x1/w) * this.yearRange + this.minYear);
+    var d2 = Math.round((x2/w) * this.yearRange + this.minYear);
+    if (d1 < this.minYear) d1 = this.minYear;
+    if (d2 > this.maxYear) d2 = this.maxYear;
+    this.$dateStart.val(d1);
+    this.$dateEnd.val(d2);
   };
 
   return Search;
