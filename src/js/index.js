bafdb32332ff9c4cd1b823eb9f91e3d610305bd5
Brian Foo <brian@youaremyjoy.org>
2016-09-15 16:17:05 -0400

    collapsible TOC

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//=include vendor/jquery-3.1.0.min.js
+
+var Index = (function() {
+  function Index(options) {
+    var defaults = {};
+    this.opt = $.extend({}, defaults, options);
+    this.loadListeners();
+  }
+
+  Index.prototype.loadListeners = function(){
+    var _this = this;
+
+    $('.toggle-sublist').on('click', function(e){
+      e.preventDefault();
+      var $link = $(this);
+      var $target = $link.closest('li').find('.sublist');
+      $link.toggleClass('active');
+      $target.toggleClass('active');
+    });
+  };
+
+  return Index;
+
+})();
+
+$(function() {
+  var indexView = new Index({});
+});
