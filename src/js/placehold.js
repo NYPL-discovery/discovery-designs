5df6fdcb97aab76f773cd0c9005f67beb31d6641
Brian Foo <brian@youaremyjoy.org>
2016-10-12 18:06:10 -0400

    place hold placeholders (#14)

---
+++ src/js/placehold.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//=include vendor/jquery-3.1.0.min.js
+//=include vendor/hammer.min.js
+//=include views/header.js
+//=include views/placehold.js
+
+$(function() {
+  var headerView = new Header({});
+  var placeholdView = new Placehold({});
+});
---
+++ src/js/views/placehold.js
@@ -0,0 +1,16 @@
+var Placehold = (function() {
+  function Placehold(options) {
+    var defaults = {};
+    this.opt = $.extend({}, defaults, options);
+    this.loadListeners();
+  }
+
+  Placehold.prototype.loadListeners = function(){
+    var _this = this;
+
+
+  };
+
+  return Placehold;
+
+})();
